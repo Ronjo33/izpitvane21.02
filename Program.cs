6432852af610b195e8882463cc697e34ee981cae
Ronjo33 <zlatomir.stefanov480@edu1.pgzaimov.com>
2023-02-21 09:45:30 +0200

    zl

--- Program.cs
+++ Program.cs
@@ -11,19 +11,57 @@ namespace _21._02._2023_izpitvane
         static void Main(string[] args)
         {
             int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            //a
+            Console.WriteLine("sredno aretmeti4no");
             ArrayMas(num);
+            Console.WriteLine();
+            //b
+            Console.WriteLine("Cislata na edin red");
             Edinred(num);
+            Console.WriteLine();
+            //v
+            Console.WriteLine("broi nuli ");
+            BroinaNuli(num);
+            Console.WriteLine();
+            //d
+            Console.WriteLine("vsi4ki 4isla ot array na edin red");
+            Novred(num);
+            Console.WriteLine();
         }
 
-        private static void Edinred(int[] num)
+        private static void Novred(int[] array)
         {
-            Console.WriteLine(num+ ",");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"array[{i + 1}] = {array[i]}");
+            }
+        }
+
+        private static void BroinaNuli(int[] nula)
+        {
+            int numnula = 0;
+            for (int i = 0; i < nula.Length; i++)
+            {
+                if (nula[i]==0)
+                {
+                    numnula++;
+                }
+                
+            }
+            Console.WriteLine(numnula);
+        }
+
+        private static void Edinred(int[] array)
+        {
+            Console.Write(string.Join(",",array));
+            Console.WriteLine();
         }
 
         private static void ArrayMas(int[] array)
         {
-            int sum = 0;
-            int avg = 0;
+            float sum = 0;
+            float avg = 0;
             for (int i = 0; i < array.Length; i++)
             {
 
@@ -32,7 +70,7 @@ namespace _21._02._2023_izpitvane
                 
             }
             avg= sum / array.Length;
-            Console.WriteLine($"{avg}");
+            Console.WriteLine($"{avg:f2}");
         }
     }
 }
